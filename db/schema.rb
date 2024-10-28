39b0c5d802082f064f0e2dba1aec8e6e0b11cc54
Ben Purinton <ben.purinton@gmail.com>
2024-10-28 23:42:39 +0000

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2020_07_30_192221) do
+ActiveRecord::Schema[7.1].define(version: 2020_07_30_192221) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "courses", force: :cascade do |t|
     t.string "title"
     t.string "term_offered"
